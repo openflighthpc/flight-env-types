fbc0dbf430b902082b206b897870084d1999f741
Stu Franks <stu@stuts.uk>
2022-05-18 14:26:55 +0100

    Remove deprecated bootstrap

--- spack/global-install.sh
+++ spack/global-install.sh
@@ -55,4 +55,3 @@ env_stage "Bootstrapping Spack environment (spack@${name})"
 if ! which python &>/dev/null; then
   sed -i -e 's,#!/usr/bin/env python$,#!/usr/bin/env python3,g' bin/spack
 fi
-bin/spack bootstrap
